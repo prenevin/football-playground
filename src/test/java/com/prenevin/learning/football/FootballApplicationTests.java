1c1fadd4c5dfd46ea1dca13bdf81037aef95fa52
Prenevin Moodley <24520737+prenevin@users.noreply.github.com>
2022-08-09 08:05:04 +0200

    initial commit to the fun and learning :)

---
+++ src/test/java/com/prenevin/learning/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prenevin.learning.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
